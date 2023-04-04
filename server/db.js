10eaadc0f60f0b7a1ae03c76e7a5a6bd81350047
natmerem <129467877+natmerem@users.noreply.github.com>
2023-04-04 05:42:16 -0500

    Update db.js

--- server/db.js
+++ server/db.js
@@ -1,7 +1,14 @@
-const { Pool } = require("pg");
+const Pool = require("pg").Pool;
+//require("dotenv").config;
+const path = require("path");
+require("dotenv").config({ path: path.resolve(__dirname, "../.env") });
 
-const pool = new Pool();
+const pool = new Pool({
+  user: process.env.PGUSER,
+  password: process.env.PGPASSWORD,
+  host: process.env.PGHOST,
+  port: process.env.PGPORT,
+  database: process.env.PGDATABASE,
+});
 
-module.exports = {
-  query: (text, params) => pool.query(text, params),
-};
+module.exports = pool;
