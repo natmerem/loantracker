86119727c6835d00aefaab2bc593e24a0ca4fc73
Nathalie M <129467877+natmerem@users.noreply.github.com>
2023-04-08 16:53:49 -0500

    Update ListItem.js

--- client/src/components/ListItem.js
+++ client/src/components/ListItem.js
@@ -1,14 +1,20 @@
 import React, { useState } from "react";
+// the listitem component is composed of other components
+// each loan in the list of loans has a loan icon, a porgress stage, a type, a edit button/modal access notes
 import LoanIcon from "./LoanIcon";
 import ProgressBar from "./ProgressBar";
 import LoanType from "./LoanType";
 import Modal from "./Modal";
 import Notes from "./Notes";
 
+// each loan in the user specific list of loans is mapped to a individual list item
+// see App.js for loan prop
 const ListItem = ({ loan, getData }) => {
+  // by default, the notes and modal popups are not shown
   const [showNotes, setShowNotes] = useState(false);
   const [showModal, setShowModal] = useState(false);
-
+  
+  // if the user clicks the delete button, the loan is deleted by using the delete request url, see server.js
   const deleteItem = async (e) => {
     e.preventDefault();
     try {
@@ -18,11 +24,6 @@ const ListItem = ({ loan, getData }) => {
           method: "DELETE",
         }
       );
-      /*
-      const response = await fetch(`http://localhost:8000/loans/${loan.id}`, {
-        method: "DELETE",
-      });
-      */
       if (response.status === 200) {
         console.log("success");
         getData();
@@ -31,7 +32,8 @@ const ListItem = ({ loan, getData }) => {
       console.error(err);
     }
   };
-
+  // loan data from App.js gets sent to the components that make up the ListItem component
+  // show modal or notes popup when associated button is clicked
   return (
     <li className="list-item">
       <div className="info-container">
