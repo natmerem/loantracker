daf9c865642079b3666464d6445e125a5e3bac63
Nathalie M <129467877+natmerem@users.noreply.github.com>
2023-04-08 16:23:09 -0500

    Update Notes.js

--- client/src/components/Notes.js
+++ client/src/components/Notes.js
@@ -1,5 +1,7 @@
 import React from "react";
-
+// loan notes, something the user indicates when they create/edit loans, stored in loans table of db
+// the loan notes is passed as a prop from App.js to this component, thru the ListItem component
+// the loan notes is a popup/modal that is accessed by button click, included for each loan in the list of loans, see ListItem.js for import
 const Notes = ({ loan, setShowNotes }) => {
   return (
     <div className="overlay">
