45ee5c64fd5ae95d61f2157f861173177a9d9126
Nathalie M <129467877+natmerem@users.noreply.github.com>
2023-04-08 17:08:55 -0500

    Update ListHeader.js

--- client/src/components/ListHeader.js
+++ client/src/components/ListHeader.js
@@ -3,15 +3,20 @@ import Modal from "./Modal";
 import { useCookies } from "react-cookie";
 
 const ListHeader = ({ listName, getData }) => {
+  // by default, the modal popup is not shown
   const [showModal, setShowModal] = useState(false);
+  // no user email cookie, no loan list. no auth token cookie, get sent back to sign in page
   const [cookies, setCookie, removeCookie] = useCookies(null);
-
+  
+  // function triggered if user clicks sign out button
+  // remove cookies, reload page, user will see the sign in page
   const signOut = () => {
     console.log("signout");
     removeCookie("Email");
     removeCookie("AuthToken");
     window.location.reload();
   };
+  // click the add loan button and the modal popup will be displayed
   return (
     <div className="list-header">
       <h1>{listName}</h1>
