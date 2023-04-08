6cdb2b00bee6cf9bc705995b4b74eda8d3b1d83e
Nathalie M <129467877+natmerem@users.noreply.github.com>
2023-04-08 14:52:35 -0500

    Update db.js

--- server/db.js
+++ server/db.js
@@ -1,8 +1,10 @@
 const Pool = require("pg").Pool;
 //require("dotenv").config;
+// local path to .env, irrelevant for render.com
 const path = require("path");
 require("dotenv").config({ path: path.resolve(__dirname, "../.env") });
 
+// create connection to postgres db, pg connection env variables configured on render.com
 const pool = new Pool({
   user: process.env.PGUSER,
   password: process.env.PGPASSWORD,
