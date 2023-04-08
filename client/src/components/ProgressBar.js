890eb0cf0d9f6f80ca7ed75a85564390dfbbfd0f
Nathalie M <129467877+natmerem@users.noreply.github.com>
2023-04-08 16:20:38 -0500

    Update ProgressBar.js

--- client/src/components/ProgressBar.js
+++ client/src/components/ProgressBar.js
@@ -1,5 +1,7 @@
 import React from "react";
-
+// loan stage/progress, something the user indicates when they create/edit loans, stored in loans table of db
+// the loan progress is passed as a prop from App.js to this component, thru the ListItem component
+// the loan progress is included for each loan in the list of loans, see ListItem.js for import
 const ProgressBar = ({ progress }) => {
   const calcProg = (progress / 8) * 100;
   return (
