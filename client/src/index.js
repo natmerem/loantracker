41eb7509940e604d7fef106f16f17ee0e11c27e4
Nathalie M <129467877+natmerem@users.noreply.github.com>
2023-04-08 15:07:32 -0500

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-
+// inject everything from App.js into #root element of index.html
+// and style everything within App.js with rules from index.css
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
